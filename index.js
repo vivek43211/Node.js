4823e386bc81ba56ea190489eae3e2148dbb05fe
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-09-12 21:52:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const lovish = require("./second")
-console.log("hello worlds, my name is vivek", lovish)
+const fs  = require("fs")
+
+// fs.writeFile("read.txt","this is my async file " , (err)=>{
+
+//     console.log("file is created ")
+//     console.log(err)
+// })
+
+fs.appendFile("read.txt","and it have ability to handle mutliple taska simultanesulu",(err)=>{
+console.log("file is applended")
+})
+
+fs.readFile("read.txt","utf-8",(err,data)=>{
+    console.log(data)
+})
